62a664dffe0b8024e10d8a52f7ad12ad9fe4d2d0
Anupal Mishra <anupal.pm@gmail.com>
2023-01-06 17:07:58 +0000

    modify pinhole rule to include only wlan0

--- firewall-rules.sh
+++ firewall-rules.sh
@@ -3,7 +3,7 @@
 # Firewall commands to stop all incoming traffic from WiFi
 iptables -A INPUT -p tcp --destination-port 53 -m comment --comment "Allow DNS over TCP" -j ACCEPT -i wlan0
 iptables -A INPUT -p udp --destination-port 53 -m comment --comment "Allow DNS over UDP" -j ACCEPT -i wlan0
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT -i wlan0
 iptables -A INPUT -j DROP -i wlan0
-ip6tables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+ip6tables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT -i wlan0
 ip6tables -A INPUT -j DROP -i wlan0
